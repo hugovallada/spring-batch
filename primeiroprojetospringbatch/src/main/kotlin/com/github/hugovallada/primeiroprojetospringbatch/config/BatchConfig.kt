908a727e854b479ec2f9ef6789b6c185d2cbebf9
Hugo Vallada <valladahugo@gmail.com>
2023-05-21 09:51:20 -0300

    Múltipla execução de Jobs

--- primeiroprojetospringbatch/src/main/kotlin/com/github/hugovallada/primeiroprojetospringbatch/config/BatchConfig.kt
+++ primeiroprojetospringbatch/src/main/kotlin/com/github/hugovallada/primeiroprojetospringbatch/config/BatchConfig.kt
@@ -2,11 +2,13 @@ package com.github.hugovallada.primeiroprojetospringbatch.config
 
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
-import org.springframework.batch.core.StepContribution
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
-import org.springframework.batch.core.scope.context.ChunkContext
+import org.springframework.batch.core.configuration.annotation.StepScope
+import org.springframework.batch.core.launch.support.RunIdIncrementer
+import org.springframework.batch.core.step.tasklet.Tasklet
 import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -16,22 +18,27 @@ class BatchConfig(
     private val stepBuilderFactory: StepBuilderFactory,
 ) {
 
-
     @Bean
     fun imprimeOlaJob(): Job {
         return jobBuilderFactory["imprimeOlaJob"]
             .start(imprimeOlaStep())
+            .incrementer(RunIdIncrementer())
             .build()
     }
 
     private fun imprimeOlaStep(): Step {
         return stepBuilderFactory["imprimeOlaStep"]
-            .tasklet { _: StepContribution, _: ChunkContext ->
-                println("Ola, mundo!")
-                RepeatStatus.FINISHED
-            }
+            .tasklet(imprimeOlaTasklet())
             .build()
     }
 
+    @Bean
+    @StepScope // Precisa estar no escopo da step para usar o Value do jobParameters
+    fun imprimeOlaTasklet(@Value("#{jobParameters['nome']}") name: String? = null): Tasklet {
+        return Tasklet { _, _ ->
+            println("Ola, $name")
+            RepeatStatus.FINISHED
+        }
+    }
 
 }
